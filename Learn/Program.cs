b3ba1034e18a6cf11c400307a8a5a9e289d42b38
ctrisday <122680968+ctrisday@users.noreply.github.com>
2023-04-27 22:55:12 +0700

    ádasd
    ádasd

---
+++ Learn/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string b = "Huynh";
+        b = b + " Cao Tri";
+        StringBuilder a = new StringBuilder("How");
+        Console.WriteLine(a.Append("Tri"));
+        Console.WriteLine(b);
+    }
+}
